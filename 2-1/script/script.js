1733daf84310f02c0bc666d885f31dbbaf7fd226
渡辺誠大 <answer.is.near.2@gmail.com>
2021-03-23 21:34:42 +0900

    2-15

--- 2-1/script/script.js
+++ 2-1/script/script.js
@@ -1,8 +1,13 @@
 const app = new Vue({
+	// el :"#app"でhtml要素を指定している
   el: '#app',
+  // おなじみdata関数
   data() {
     return {
-      text: 'hello Vue.js'
+      text: 'hello'
     };
   }
 });
+
+// appのtext要素を文字列で書き換えている。
+app.text = 'goodbye';
